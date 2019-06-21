c5ed1ed255155d51ba283ef0baa23997156b810b
Yagna Talakola <talakola.yagna@gmail.com>
2019-06-21 13:38:20 -0500

    added Java+Kafka scaffolding

---
+++ src/main/java/com/challenge/ddos/Application.java
@@ -0,0 +1,16 @@
+package com.challenge.ddos;
+
+import com.talakola.kafka.producer.ShipmentTransitProducer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        ShipmentTransitProducer producer = ctx.getBean(ShipmentTransitProducer.class);
+        producer.produce();
+    }
+}
